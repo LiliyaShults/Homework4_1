a56aa36188e9fd77f2b63c3503cbb42a6a6264aa
Your Name <you@example.com>
2024-01-06 00:25:59 +0300

    init

---
+++ src/test/java/TestWithHeadless.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestWithHeadless {
+    WebDriver driver;
+
+    @BeforeAll
+    public static void driverSetup() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void driverStart() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void driverStop() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void headless() {
+
+        driver.get("https://duckduckgo.com/");
+        driver.findElement(By.cssSelector("[id=searchbox_input]"))
+                .sendKeys("ОТУС" + Keys.ENTER);
+
+        String str1 ="Онлайн‑курсы для профессионалов, дистанционное обучение современным ...";
+        String str2 = driver.findElement(By.xpath("//*[@id=\"r1-0\"]/div[2]/h2/a/span")).getText();
+        if (str1.equals(str2)){
+            System.out.println("Искомый текст- совпадает: Онлайн‑курсы для профессионалов, дистанционное обучение современным ... ");
+        }else{
+            System.out.println("False. Искомый текст не совпадает");
+        }
+
+    }
+
+}
